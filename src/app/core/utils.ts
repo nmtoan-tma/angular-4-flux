8cf0de03b23cf693654e046910a169a811535497
tttlan <tttlan@tma.com.vn>
2017-11-03 18:47:49 +0700

    build skeleton

--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -1,5 +1,16 @@
-const typeCache: { [label: string]: boolean } = {};
+import { RouterStateSerializer } from '@ngrx/router-store';
+import { RouterStateSnapshot, Params } from '@angular/router';
 
+/**
+ * This function coerces a string into a string literal type.
+ * Using tagged union types in TypeScript 2.x, this enables powerful 
+ * typerchecking ot our reducers.
+ * 
+ * Since every action label passes through this function it is a good place 
+ * to ensure all of our action labels are unique 
+ */
+
+const typeCache: { [label: string]: boolean } = {};
 export function type<T>(label: T | ''): T {
     if (typeCache[<string>label]) {
         throw new Error(`Action type '${label}' is not unique`);
@@ -8,4 +19,12 @@ export function type<T>(label: T | ''): T {
     typeCache[<string>label] = true;
 
     return <T>label;
+}
+
+/**
+ * 
+ */
+
+export class RouterStateUrl {
+
 }
--- src/app/reducers/app.reducer.ts
+++ src/app/reducers/app.reducer.ts
@@ -4,7 +4,23 @@ import {
     createSelector
 } from '@ngrx/store';
 import { compose } from '@ngrx/core/compose';
-import { routerReducer, RouterReducerState, RouterStateSerializer } from '@ngrx/router-store';
+import * as fromRouterStore from '@ngrx/router-store';
 import { storeFreeze } from 'ngrx-store-freeze';
 
 import { AppSettings } from '../constants/AppSettings';
+import { RouterStateUrl } from '../core/utils';
+
+// ----------------------------- User reducers -----------------------------------------------//
+
+import * as fromUserReducer from './user.reducer';
+
+/**
+ * As mentioned, we treat each reducer like a table in a database. This mean our top level state interface
+ * is just a map of keys to inner state types
+ */
+
+export interface State {
+    router: fromRouterStore.RouterReducerState<RouterStateUrl>;
+}
+
+
